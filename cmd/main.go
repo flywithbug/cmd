e62680deeea7ef1e544f25df741921ccfeb61d43
flywithbug <flywithbug@gmail.com>
2019-06-28 17:22:25 +0800

    cmd

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 
+	"os"
+	"time"
+
 	"github.com/flywithbug/cmd"
 )
 
 func main() {
-	b, _ := cmd.Exec("ls")
-	fmt.Println(string(b))
+	// Create Cmd, buffered output
+	envCmd := cmd.NewCmd("env")
+
+	// Run and wait for Cmd to return Status
+	status := <-envCmd.Start()
+
+	// Print each line of STDOUT from Cmd
+	for _, line := range status.Stdout {
+		fmt.Fprintf(os.Stdout, line)
+		time.Sleep(time.Second * 1)
+	}
+
 }
